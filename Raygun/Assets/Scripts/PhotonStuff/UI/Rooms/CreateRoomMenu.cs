6f84ae7146164637ed0822f8782cb36229b51bf2
graves_cigar <michigladh@web.de>
2019-08-26 18:03:57 +0200

    Added Lobbysystem kinda on Raygun and added new Sticc game

---
+++ Raygun/Assets/Scripts/PhotonStuff/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,38 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text _roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (PhotonNetwork.IsConnected && _roomName.text.Length > 0)
+        {
+            RoomOptions roomOptions = new RoomOptions()
+            {
+                MaxPlayers = 20,
+                PlayerTtl = 600000,
+                EmptyRoomTtl = 0
+            };
+            PhotonNetwork.JoinOrCreateRoom(_roomName.text, roomOptions, TypedLobby.Default);
+        }
+        else
+            return;
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Created room successfully.");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("Failed to create room.");
+    }
+}
---
+++ Raygun/Assets/Scripts/PhotonStuff/manager/TestConnect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to Server!");
+        print(PhotonNetwork.LocalPlayer.NickName);
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from Server! Reason: " + cause.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        print("Joined Lobby");
+    }
+}
